d14bb0137ad91d99afd04c4329939fa168d331ad
Catalin Brassoi <cbrassoi@talentingsoftware.com>
2022-10-16 19:36:51 +0300

    Now we can navigate through multiple possible pages; added styles to buttons which selects new pages.

---
+++ SevenSlots/SevenSlots/SevenSlots/View/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using NavigationPage = Xamarin.Forms.NavigationPage;
+
+namespace SevenSlots
+{
+    public partial class MainPage : Xamarin.Forms.MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Detail = new NavigationPage(new Page1());
+
+            IsPresented = false;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page1());
+
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page2());
+
+            IsPresented = false;
+        }
+    }
+}
