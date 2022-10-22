9f7fa8c986b9cd827002a360b17f19fb0e29de2a
Tinel Valentin CARMACIU <tinel.carmaciu@student.unitbv.ro>
2022-10-22 14:58:03 +0300

    Added accordion button with a coroutine animation

---
+++ SevenSlots/SevenSlots/SevenSlots/ViewModel/HomePageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using SevenSlots.Commands;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace SevenSlots.ViewModel
+{
+    internal class HomePageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand AccordionAnimationCommand { get; }
+        private string accordionText;
+        public string AccordionText
+        {
+            get { return accordionText; }
+            set
+            {
+                accordionText = value;
+                OnPropertyChanged(nameof(AccordionText)); // test
+            }
+        }
+
+        public HomePageViewModel()
+        {
+            AccordionText = "TestValue";
+            AccordionAnimationCommand = new AccordionAnimationCommand();
+        }
+
+        
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
