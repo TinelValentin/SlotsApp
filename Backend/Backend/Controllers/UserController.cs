e0acc04c19d8ee4f8138fc9025d1ea8ff91226e2
Sorin37 <glizard.glizard@yahoo.com>
2022-10-22 14:22:34 +0300

    Created User model and controller

---
+++ Backend/Backend/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+
+        static List<User> _users = new List<User> {
+            new User { Id=Guid.NewGuid(),
+            Username="Tainal",
+            Password="Zanussi",
+            FirstName="Tinel",
+            LastName="Carmaciu",
+            Email="tinel_vali@yahoo.com",
+            SSN="5010614100145",
+            Phone="0763217542",
+            Wallet=10000000}
+        };
+
+        [HttpGet]
+        public IActionResult GetNotes()
+        {
+            return Ok(_users);
+        }
+
+    }
+}
---
+++ Backend/Backend/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Backend.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string SSN { get; set; }
+        public double Wallet { get; set; }
+        public byte[] IdCard { get; set; }
+    }
+}
