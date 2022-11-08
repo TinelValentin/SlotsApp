4c58a43c204510046993f8a7c478bb3d7d7fdcb3
Catalin Brassoi <cbrassoi@talentingsoftware.com>
2022-11-08 15:11:59 +0200

    Created renderers in IOs just like in Android.

---
+++ SevenSlots/SevenSlots/SevenSlots.iOS/Renderers/CustomInputBoxRendererIOs.cs
@@ -0,0 +1,34 @@
+﻿using Foundation;
+using SevenSlots.Controls;
+using SevenSlots.iOS.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer (typeof(CustomInputBox), typeof(CustomInputBoxRendererIOs))]
+namespace SevenSlots.iOS.Renderers
+{
+    public class CustomInputBoxRendererIOs : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            //Configure native control (TextBox)
+            if (Control != null)
+            {
+                Control.Background = null;
+            }
+
+            // Configure Entry properties
+            if (e.NewElement != null)
+            {
+
+            }
+        }
+    }
+}
