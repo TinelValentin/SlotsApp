9a726ba0e4ceef6466f861e5c7020f3c17316df2
Catalin Brassoi <cbrassoi@talentingsoftware.com>
2022-11-21 19:04:40 +0200

    Added commands for betting an amount in BlackjackView.

--- SevenSlots/SevenSlots/SevenSlots/ViewModel/BlackjackViewModel.cs
+++ SevenSlots/SevenSlots/SevenSlots/ViewModel/BlackjackViewModel.cs
@@ -6,30 +6,61 @@ using Xamarin.Forms;
 
 namespace SevenSlots.ViewModel
 {
-    public class BlackjackViewModel
+    public class BlackjackViewModel : ViewModelBase
     {
         #region Constructors...
         public BlackjackViewModel()
         {
+            TotalBet = 0;
+            OwnedMoney = 12345;
+            TotalWin = 0;
             InitializeCommands();
         }
         #endregion
 
+        #region Private Fields...
+        private int _totalBet;
+        private int _ownedMoney;
+        private int _totalWin;
 
-        #region Fields...
-        public ICommand BetIncreaseCommand { get; set; }
         #endregion
 
+        #region Public Fields and Commands...
+        public int TotalBet
+        {
+            get { return _totalBet; }
+            set { _totalBet = value; OnPropertyChanged(); }
+        }
+        public int OwnedMoney
+        {
+            get { return _ownedMoney; }
+            set { _ownedMoney = value; OnPropertyChanged(); }
+        }
+        public int TotalWin
+        {
+            get { return _totalWin; }
+            set { _totalWin = value; OnPropertyChanged(); }
+        }
+
+        public ICommand BetIncreaseCommand { get; set; }
+        public ICommand BetDecreaseCommand { get; set; }
+        #endregion
 
         #region Private Methods...
         private void InitializeCommands()
         {
             BetIncreaseCommand = new Command(BetIncrease);
+            BetDecreaseCommand = new Command(BetDecrease);
         }
-
-        private void BetIncrease()
+        private void BetIncrease(object param)
         {
-
+            TotalBet += 10;
+            OwnedMoney -= 10;
+        }
+        private void BetDecrease(object param)
+        {
+            TotalBet -= 10;
+            OwnedMoney += 10;
         }
         #endregion
     }
