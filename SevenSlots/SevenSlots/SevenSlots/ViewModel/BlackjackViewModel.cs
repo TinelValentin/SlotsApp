27f3cf8dafeb3d100ffc517a302ad051e75fd6c9
Catalin Brassoi <cbrassoi@talentingsoftware.com>
2022-11-21 17:41:23 +0200

    Added an example for a command in BlackjackViewModel.

--- SevenSlots/SevenSlots/SevenSlots/ViewModel/BlackjackViewModel.cs
+++ SevenSlots/SevenSlots/SevenSlots/ViewModel/BlackjackViewModel.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace SevenSlots.ViewModel
 {
     public class BlackjackViewModel
     {
+        #region Constructors...
+        public BlackjackViewModel()
+        {
+            InitializeCommands();
+        }
+        #endregion
 
+
+        #region Fields...
+        public ICommand BetIncreaseCommand { get; set; }
+        #endregion
+
+
+        #region Private Methods...
+        private void InitializeCommands()
+        {
+            BetIncreaseCommand = new Command(BetIncrease);
+        }
+
+        private void BetIncrease()
+        {
+
+        }
+        #endregion
     }
 }
