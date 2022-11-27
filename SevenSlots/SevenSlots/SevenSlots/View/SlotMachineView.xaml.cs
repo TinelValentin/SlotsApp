0876e35e38b1078759347ab2acbb08d792202ab3
Sorin37 <glizard.glizard@yahoo.com>
2022-11-27 16:56:47 +0200

    The app changes orientation on the slot machine page

---
+++ SevenSlots/SevenSlots/SevenSlots/View/SlotMachineView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Android.Content.PM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SevenSlots.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SlotMachineView : ContentPage
+    {
+        public SlotMachineView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MessagingCenter.Send(this, "AllowLandscape");
+        }
+
+        //during page close setting back to portrait
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Send(this, "PreventLandscape");
+        }
+    }
+}
